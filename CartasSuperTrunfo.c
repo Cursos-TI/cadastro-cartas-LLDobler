b5c0d69813baf7ea0b85cc75339b9675acc1fbde
LLDobler <leodobler18@gmail.com>
2025-09-13 21:23:53 +0000

    Implantado o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,43 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+    char nomeEstado1[20], codigoCarta1[5], nomeCidade1[20];
+  
+  // Aqui ocorrerá a entrada dos dados fornecidos pelo usuário.
+  printf("Insira os dados da primeira carta!\n\n");
+  printf("Digite o nome do Estado:  \n");
+  scanf("%s", &nomeEstado1);
+
+  printf("Digite o código da carta:  \n");
+  scanf("%s", &codigoCarta1);
+
+  printf("Digite o nome da Cidade:  \n");
+  scanf("%s", &nomeCidade1);
+
+  printf("Digite a quantidade da população nesta cidade:  \n");
+
+  printf("Digite a área em quilômetros quadrados:  \n");
+
+  printf("Digite o PIB da cidade:  \n");
+
+  printf("Digite a quantidade de pontos turísticos da cidade:  \n\n");
+
+  printf("Carta 1 salva com sucesso!\n\n Agora, introduza os dados da segunda carta!\n\n");
+
+  printf("Digite o nome do Estado:  \n");
+
+  printf("Digite o nome da Cidade:  \n");
+
+  printf("Digite a quantidade da população nesta cidade:  \n");
+
+  printf("Digite a área em quilômetros quadrados:  \n");
+
+  printf("Digite o PIB da cidade:  \n");
+
+  printf("Digite a quantidade de pontos turísticos da cidade:  \n");
+  
+  
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
